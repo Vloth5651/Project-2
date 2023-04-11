7c98872ac913203499bda620d6173b302874c50a
Vloth5651 <125697905+Vloth5651@users.noreply.github.com>
2023-04-11 11:15:22 -0400

    Add files via upload

--- payment details.py
+++ payment details.py
@@ -2,18 +2,23 @@ from tkinter import ttk
 from tkinter import *
 import tkinter.font
 from tkinter import messagebox
+import subprocess
+
+import sqlite3
 
 
 window = tkinter.Tk()
 window.title("Ready to Pay?")
-window.minsize(width=500, height=700)
+window.minsize(width=500, height=600)
 window.configure(bg="LightPink1")
 
 frame = tkinter.Frame(window)
-frame.pack()
+frame.pack(pady=10)
 
-img = PhotoImage(file='cake.gif')
-Label(window, image=img, bg='white').place(x=300, y=300)
+img = PhotoImage(file='cake.gif').subsample(2, 2)
+img_label= Label(window, image=img, bg='white')
+img_label.pack(anchor="center", padx=10, pady=10)
+
 
 
 # saving payment info
@@ -48,13 +53,38 @@ csv_number_entry = tkinter.Entry(payment_info_frame)
 csv_number_entry.grid(row=8, column=0)
 
 # expiry number
-expiry_number = tkinter.Label(payment_info_frame, text= "Expiry")
+expiry_number = tkinter.Label(payment_info_frame, text= "Expiry (format: YYYY-MM-DD)")
 expiry_number.grid(row=9, column=0)
 
 expiry_number_entry = tkinter.Entry(payment_info_frame)
 expiry_number_entry.grid(row=10, column=0)
 
-Button(window, text="Submit Order", width=10).place(x=200,y=400)
+def save_details():
+    selected_method = card_combobox.get()
+    name = card_name_entry.get()
+    card_num = int(card_number_entry.get())
+    csv_num = int(csv_number_entry.get())
+    expiry_date = expiry_number_entry.get()
+
+    # SQLite database and table
+    conn = sqlite3.connect("cakeorder_db.db")
+    c = conn.cursor()
+    c.execute("INSERT INTO payment_details (name, pay_method, card_num, csv, expiry) VALUES (?, ?, ?, ?, ?)", (name, selected_method, card_num, csv_num, expiry_date))
+    conn.commit()
+    conn.close()
+
+     #create_new_window()
+    proc=subprocess.Popen(["python", "ThankYou.py"])
+    #close the current GUI
+    window.destroy()
+    # allows the new_gui to stay
+    proc.wait()
+
+submit_button = Button(window, text="Submit Order", width=10, command=save_details)
+submit_button.pack(anchor="center")
+
+
+
 window.mainloop()
 
 
